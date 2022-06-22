2e95effdf139624194cb2de336cf05c3303b4953
kobe-antonio <antoni.vidal@kobe.io>
2022-06-22 03:56:31 -0300

    Dagger data module adjustment

--- pynne/app/src/main/java/com/example/pynne/ui/di/MainModule.kt
+++ pynne/app/src/main/java/com/example/pynne/ui/di/MainModule.kt
@@ -2,8 +2,8 @@ package com.example.pynne.ui.di
 
 import androidx.lifecycle.ViewModel
 import com.example.pynne.di.ViewModelKey
-import com.example.pynne.viewmodel.MatchViewModel
-import com.example.pynne.viewmodel.RankingViewModel
+import com.example.pynne.viewmodel.MainViewModel
+import com.example.pynne.viewmodel.MatchInputViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -13,11 +13,11 @@ interface MainModule {
 
     @Binds
     @IntoMap
-    @ViewModelKey(RankingViewModel::class)
-    fun bindRankingViewModel(viewModel: RankingViewModel): ViewModel
+    @ViewModelKey(MatchInputViewModel::class)
+    fun bindRankingViewModel(viewModel: MatchInputViewModel): ViewModel
 
     @Binds
     @IntoMap
-    @ViewModelKey(MatchViewModel::class)
-    fun bindMatchViewModel(viewModel: MatchViewModel): ViewModel
+    @ViewModelKey(MainViewModel::class)
+    fun bindMatchViewModel(viewModel: MainViewModel): ViewModel
 }
